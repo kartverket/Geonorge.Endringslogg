6cf4d5e87a0fe5d6d7a5ea50ea8718a02858b099
Henning Jensen <henning@arkitektum.no>
2018-02-26 14:59:13 +0100

    Add datamodel

---
+++ Web/Models/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Web.Models
+{
+    public class LogEntry
+    {
+        public string Uuid { get; set; }
+        public DateTime DateTime { get; set; }
+        public string ApplicationId { get; set; }
+        public string User { get; set; }
+        public string Description { get; set; }
+        public string Operation { get; set; }
+        public string ElementId { get; set; }
+    }
+}
