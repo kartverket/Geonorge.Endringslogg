67f03528c605b8fc94424da94ace0b8c433f02be
Dag Olav Dahle <dagolav@arkitektum.no>
2018-02-28 11:28:27 +0100

    Migrate and seed database

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,20 +1,86 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Geonorge.Endringslogg.Web.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Geonorge.Endringslogg.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                var host = BuildWebHost(args);
+
+                MigrateAndSeedDatabase(host);
+
+                Log.Information("Starting web host");
+                host.Run();
+
+                return 0;
+
+            }
+
+            catch (Exception ex)
+
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+
+                return 1;
+            }
+
+            finally
+
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void MigrateAndSeedDatabase(IWebHost host)
+        {
+            Log.Information("Running migrations and seeding data");
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+
+            }
+
         }
 
         public static IWebHost BuildWebHost(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
+
         }
     }
 }
